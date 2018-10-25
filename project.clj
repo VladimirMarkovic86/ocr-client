98979bb5359eb912ec4a62bde82e5dc44ab07098
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2018-10-25 15:59:37 +0200

    Changed version of utils-lib, which has java-heap-size. Changed version of server-lib too.

--- project.clj
+++ project.clj
@@ -10,13 +10,13 @@
                  [org.clojars.vladimirmarkovic86/htmlcss-lib "0.1.0"]
                  [org.clojars.vladimirmarkovic86/js-lib "0.1.0"]
                  [org.clojars.vladimirmarkovic86/framework-lib "0.2.0"]
-                 [org.clojars.vladimirmarkovic86/utils-lib "0.1.0"]
+                 [org.clojars.vladimirmarkovic86/utils-lib "0.3.0"]
                  [org.clojars.vladimirmarkovic86/client-test-lib "0.1.0"]
                  [org.clojars.vladimirmarkovic86/language-lib "0.2.0"]
                  [org.clojars.vladimirmarkovic86/ocr-middle "0.1.0"]
                  [org.clojars.vladimirmarkovic86/common-client "0.2.0"]
                  [org.clojars.vladimirmarkovic86/common-middle "0.1.0"]
-                 [org.clojars.vladimirmarkovic86/server-lib "0.3.0"]
+                 [org.clojars.vladimirmarkovic86/server-lib "0.3.1"]
                  ]
 
   :min-lein-version "2.0.0"
