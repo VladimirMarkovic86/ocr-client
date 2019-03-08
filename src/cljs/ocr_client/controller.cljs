df0b941a584ed076dc03073ca1aeb6ba8e837a50
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-03-08 17:30:38 +0100

    Task 62: Refactored common-server, separated configuration for server and client side.

--- src/cljs/ocr_client/controller.cljs
+++ src/cljs/ocr_client/controller.cljs
@@ -1,65 +1,14 @@
 (ns ocr-client.controller
-  (:require [ajax-lib.core :refer [ajax base-url with-credentials]]
-            [websocket-lib.core :refer [base-ws-url]]
-            [ocr-client.html :as ht]
-            [ocr-middle.functionalities :as fns]
+  (:require [ajax-lib.core :refer [ajax]]
+            [ocr-client.config :as config]
             [common-middle.request-urls :as rurls]
-            [common-client.role.entity :as re]
             [common-client.login.controller :refer [redirect-to-login
-                                                    main-page
-                                                    logout
-                                                    custom-menu
-                                                    home-page-content
-                                                    logout-fn
-                                                    logout-success
-                                                    logout-success-fn]]))
+                                                    main-page]]))
 
 (defn am-i-logged-in
   "Check if session is active"
   []
-  (let [base-uri (.-baseURI
-                   js/document)
-        base-uri (if (< -1
-                        (.indexOf
-                          base-uri
-                          "herokuapp"))
-                   "https://ocr-server-clj.herokuapp.com"
-                   "https://192.168.1.86:1602")]
-    (reset!
-      base-url
-      base-uri))
-  (reset!
-    with-credentials
-    true)
-  (let [base-ws-uri (.-baseURI
-                      js/document)
-        base-ws-uri (if (< -1
-                           (.indexOf
-                             base-ws-uri
-                             "herokuapp"))
-                      "wss://ocr-server-clj.herokuapp.com"
-                      "wss://192.168.1.86:1602")]
-    (reset!
-      base-ws-url
-      base-ws-uri))
-  #_(reset!
-    base-url
-    "/clojure")
-  (reset!
-    custom-menu
-    ht/custom-menu)
-  (reset!
-    home-page-content
-    (ht/home-page-content))
-  (reset!
-    logout-fn
-    logout)
-  (reset!
-    logout-success-fn
-    logout-success)
-  (reset!
-    re/functionalities
-    fns/functionalities)
+  (config/setup-environment)
   (ajax
     {:url rurls/am-i-logged-in-url
      :success-fn main-page
