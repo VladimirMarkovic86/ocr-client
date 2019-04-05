d9cabaf375de04801770bab02eaaea087b6a7677
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-04-05 16:23:51 +0200

    Task65: Testing pdflatex on heroku.

--- project.clj
+++ project.clj
@@ -16,7 +16,7 @@
                  [org.clojars.vladimirmarkovic86/ocr-middle "0.3.5"]
                  [org.clojars.vladimirmarkovic86/common-client "0.3.24"]
                  [org.clojars.vladimirmarkovic86/common-middle "0.2.5"]
-                 [org.clojars.vladimirmarkovic86/server-lib "0.3.24"]
+                 [org.clojars.vladimirmarkovic86/server-lib "0.3.25"]
                  [org.clojars.vladimirmarkovic86/validator-lib "0.1.18"]
                  ]
 
