cf5cc87333b0ff471e199f493c8c696792271c0f
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-06-18 15:49:50 +0200

    Task 63: Implemented tests for validator-lib.core namespace.

--- project.clj
+++ project.clj
@@ -9,15 +9,15 @@
                  [org.clojars.vladimirmarkovic86/websocket-lib "0.1.2"]
                  [org.clojars.vladimirmarkovic86/htmlcss-lib "0.1.6"]
                  [org.clojars.vladimirmarkovic86/js-lib "0.1.15"]
-                 [org.clojars.vladimirmarkovic86/framework-lib "0.2.39"]
+                 [org.clojars.vladimirmarkovic86/framework-lib "0.2.40"]
                  [org.clojars.vladimirmarkovic86/utils-lib "0.4.9"]
                  [org.clojars.vladimirmarkovic86/client-test-lib "0.2.12"]
                  [org.clojars.vladimirmarkovic86/language-lib "0.2.30"]
                  [org.clojars.vladimirmarkovic86/ocr-middle "0.3.8"]
-                 [org.clojars.vladimirmarkovic86/common-client "0.3.36"]
+                 [org.clojars.vladimirmarkovic86/common-client "0.3.37"]
                  [org.clojars.vladimirmarkovic86/common-middle "0.2.8"]
                  [org.clojars.vladimirmarkovic86/server-lib "0.3.30"]
-                 [org.clojars.vladimirmarkovic86/validator-lib "0.1.28"]
+                 [org.clojars.vladimirmarkovic86/validator-lib "0.1.29"]
                  ]
 
   :min-lein-version "2.0.0"
