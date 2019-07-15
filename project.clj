589ef325b7ac4b256c934821f4761fec6a2af838
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-07-15 12:20:32 +0200

    Task 88: Removed public and certificate folders from server-lib project jar package.

--- project.clj
+++ project.clj
@@ -16,7 +16,7 @@
                  [org.clojars.vladimirmarkovic86/ocr-middle "0.3.9"]
                  [org.clojars.vladimirmarkovic86/common-client "0.3.43"]
                  [org.clojars.vladimirmarkovic86/common-middle "0.2.9"]
-                 [org.clojars.vladimirmarkovic86/server-lib "0.3.31"]
+                 [org.clojars.vladimirmarkovic86/server-lib "0.3.32"]
                  [org.clojars.vladimirmarkovic86/validator-lib "0.1.34"]
                  ]
 
